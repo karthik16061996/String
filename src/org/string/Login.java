98415133c7c4b7fadd38d91fc59e314bf5c182d6
karthik16061996 <karthiknaik005@gmail.com>
2022-11-20 15:26:35 +0530

    string

---
+++ src/org/string/Login.java
@@ -0,0 +1,44 @@
+package org.string;
+
+public class Login {
+public static void main(String[] args) {
+	String s = "welcome";
+	System.out.println(s);
+	
+	// number of letters in length in string
+	int le = s.length();
+	System.out.println(le);
+	
+	// string start same or not
+	boolean b= s.startsWith("wel");
+	System.out.println(b);
+	
+	// string end same or not
+	boolean e = s.endsWith("co");
+	System.out.println(e);
+	
+	// string contains same or not
+	boolean d = s.contains("co");
+	System.out.println(d);
+	
+	// find the particular index letter
+	int p = s.indexOf("l");
+	System.out.println(p);
+	
+	// find the last index letter
+	int q = s.lastIndexOf("e");
+	System.out.println(q);
+	
+	// change one letter to another letter
+	String i = s.replaceAll("e","lol");
+	System.out.println(i);
+	
+	// change all letter of upper case
+	String up = s.toUpperCase();
+	System.out.println(up);
+	
+	// change all letter of lower case
+	String lw = s.toLowerCase();
+	System.out.println(lw);
+}
+}
